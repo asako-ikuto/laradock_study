640450fb1b238ab36754943c485faffedf86638e
ikuto-asako <m131932@gmail.com>
2022-05-11 07:41:20 +0900

    laravel課題3-4修正20220511

--- app/app/resources/views/layouts/tasklistapp.blade.php
+++ app/app/resources/views/layouts/tasklistapp.blade.php
@@ -39,23 +39,6 @@
         <input type="hidden" name="status" value="1">
         <input type="submit" value="追加">
     </form>
-
-    {{-- <script>
-        'use strict';
-        const filterStatus = document.getElementsByName('filter-status');
-
-        filterStatus.forEach((e) => {
-            e.addEventListener('change', () => {
-                if (filterStatus[0].checked) {
-                    window.location.href = '/tasks/all';
-                } else if (filterStatus[1].checked) {
-                    window.location.href = '/tasks/working';
-                } else if (filterStatus[2].checked) {
-                    window.location.href = '/tasks/finished';
-                }
-            });
-        });
-    </script> --}}
     <script src="{{ asset('/js/main.js') }}"></script>
 
 </body>
